3d1b9322eca00420f454dd810b752b29768a0666
Elvis Suffi Pompeu <elvis.suffipompeu@hotmail.com>
2015-04-17 15:03:35 -0300

    bacula.volume.gravacao.sh
    bacula.volume.gravacao.sh: exibe o status do volume de gravação.

---
+++ bacula.volume.gravacao.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Autor: Elvis Pompeu
+# Consultor Linux
+# 4Linux - Open Software Specialists
+
+BACULARESUL=$(echo "status director" | /sbin/bconsole | grep $1 | grep -i "is waiting for an appendable Volume" | wc -l)
+
+if [ ${BACULARESUL} -eq 0 ]; then
+
+        echo "0"
+
+else
+
+        echo "1"
+
+fi
